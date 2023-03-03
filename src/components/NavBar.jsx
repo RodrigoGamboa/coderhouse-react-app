92ab8bf25d97fcbd7d0d3112fd577a66c3c2eba1
Rodrigo Gamboa <rodrigogamboatorres@gmail.com>
2023-03-03 02:59:53 -0600

    fix: grouped navbar elements to right

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,16 +11,18 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <Link to="/">Vinyl Store</Link>
-      <Dropdown />
-      <Link to="/tracker">
-        <button className='flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>
-          <TruckIcon className='h-6 w-6 mr-2 text-blue-500' />
-			    <span>Tracker</span>
-		    </button>
-      </Link>
-      <Link to="/cart">
-        <CartWidget itemQ={cart} />
-      </Link>
+      <div className="flex items-center gap-2.5">
+        <Dropdown />
+        <Link to="/tracker">
+          <button className='flex bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'>
+            <TruckIcon className='h-6 w-6 mr-2 text-blue-500' />
+            <span>Tracker</span>
+          </button>
+        </Link>
+        <Link to="/cart">
+          <CartWidget itemQ={cart} />
+        </Link>
+      </div>
     </nav>
   );
 };
